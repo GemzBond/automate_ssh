ae06410b2d51c9e36d8546efa4b94f83bcd4048b
GemzBond <regem.semilla123@gmail.com>
2021-04-15 21:23:20 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import platform
+
+with open("ip_list.txt") as file:
+    dump = file.read()
+    dump = dump.splitlines()
+    current_os = platform.system().lower()
+    homedir = os.environ['HOME']
+
+    if current_os == "windows":
+        param = "-n"
+        output = "\Desktop\output.txt"
+    else:
+        param = "-c"
+        output = "/Desktop/output.txt"
+    
+    for ip in dump:
+        print(ip)
+        res = os.popen(f"ping {ip} {param} 3").read()
+        file_output = open(f'{homedir}{output}', 'a')
+        file_output.write(res)
+        file_output.close()
+
