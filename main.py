13706dd9c469de7681bcfa2b45efaafd93dc8a9c
Gemnesis <GemzBond>
2021-04-23 07:23:02 +0800

    fixed issue in windows

--- main.py
+++ main.py
@@ -18,17 +18,27 @@ with open("ip_list_copy.txt") as file:
         output = "/Desktop/output.txt"
         homedir = os.environ['HOME']
     
-    client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy())
+    
 
     for ip in dump:
         print(ip)
-        client.connect(ip, username='your_user', password='your_pass')
-        stdin, stdout, stderr = client.exec_command(f'ping {param} 3 x.x.x.x')
+        client = SSHClient()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+        client.connect(ip, username='your_username', password='your_pass')
+        stdin, stdout, stderr = client.exec_command(f'ping -c 3 x.x.x.x')
         res = f'STDOUT: |{ip}| {stdout.read().decode("utf8")}'
+        print(f'STDERR: {stderr.read().decode("utf8")}')
         file = open(f'{homedir}/Desktop/output.txt', 'a')
         file.write(res)
         file.close()
+    
+    # Because they are file objects, they need to be closed
+    stdin.close()
+    stdout.close()
+    stderr.close()
+
+    # Close the client itself
+    client.close()
 
 
 
